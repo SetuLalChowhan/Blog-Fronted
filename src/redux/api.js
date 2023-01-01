7b4438d82c7a390990fdf5bf2f065a666a2531d6
Setulal Chowhan <setulalchowhan@gmail.com>
2023-01-01 19:10:28 +0600

    s

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://dashboard.render.com" });
+const API = axios.create({ baseURL: "https://blog-for-you.onrender.com" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
     req.headers.Authorization = `Bearer ${
